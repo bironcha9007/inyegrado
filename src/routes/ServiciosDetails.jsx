be8ac9c8594ace825330267fc1b32db975412245
Bayron Castillo <bayroncastillo900725@gmail.com>
2024-11-20 19:28:06 -0500

    com

--- src/routes/ServiciosDetails.jsx
+++ src/routes/ServiciosDetails.jsx
@@ -14,44 +14,39 @@ const ServiciosDetails = () => {
                 <h1>{servicio.servicio}</h1>
                 <h3>{servicio.title}</h3>
                 <p>{servicio.info}</p>
-                {servicio.linkURL ? (
-    <button
-        style={{
-            padding: '10px 15px',
-            backgroundColor: '#007BFF',
-            border: 'none',
-            color: 'white',
-            cursor: 'pointer',
-            marginTop: '20px',
-        }}
-        onClick={() => window.open(servicio.linkURL, '_blank')}
-    >
-        Ir al Sitio Web
-    </button>
-) : (
-    <p>No hay URL disponible</p>
-)}
-             
-                <h1 style={{ marginTop: '40px' }}>Tramites</h1>
+
+
+                <h1 style={{ marginTop: '40px' }}>Información y Contacto</h1>
                 <div className="details">
-                    <div className="detail">
-                        
-                        <p><b>Expected Duration</b>: {servicio.duration}</p>
-                        <p style={{ marginTop: '15px' }}>{servicio.details}</p>
-                        <h3 style={{ marginTop: '15px' }}>Curriculum</h3>
+                    <div className="detail1">
+
+                        <p>{servicio.duration}</p>
+                        <p >{servicio.details}</p>
                         <p>{servicio.curriculum}</p>
                     </div>
-                    <img src={servicio.imageURL} alt={servicio.servicio} /> 
+                    <div className="detail">
+                        <img src={servicio.imageURL} alt={servicio.servicio} />
+                        {servicio.linkURL ? (
+                            <button
+                                style={{
+                                    padding: '10px 15px',
+                                    backgroundColor: '#007BFF',
+                                    border: 'none',
+                                    color: 'white',
+                                    cursor: 'pointer',
+                                    marginTop: '20px',
+                                }}
+                                onClick={() => window.open(servicio.linkURL, '_blank')}
+                            >
+                                Acceder
+                            </button>
+                        ) : (
+                            <p>No hay URL disponible</p>
+                        )}
+                    </div>
                 </div>
-                
-
-                <p style={{ marginTop: '20px' }}>
-                    Note that meeting these requirements does not guarantee acceptance into the servicio.
-                </p>
-
-               
             </div>
-            
+
             <Footer />
         </>
     );
